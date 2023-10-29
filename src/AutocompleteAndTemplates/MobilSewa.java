faa06b18b2e9e13ae271b3ef7c4efad80181b837
cryeass <139506951+cryeass@users.noreply.github.com>
2023-10-30 05:46:04 +0700

    init commit

---
+++ src/AutocompleteAndTemplates/MobilSewa.java
@@ -0,0 +1,48 @@
+package AutocompleteAndTemplates;
+
+/**
+ * Kelas MobilSewa digunakan untuk merepresentasikan informasi penyewaan mobil.
+ */
+public class MobilSewa {
+    private String pelanggan;
+    private String mobil;
+    private double biayaSewa;
+    private int durasiSewa;
+
+    public MobilSewa(String pelanggan, String mobil, double biayaSewa, int durasiSewa) {
+        this.pelanggan = pelanggan;
+        this.mobil = mobil;
+        this.biayaSewa = biayaSewa;
+        this.durasiSewa = durasiSewa;
+    }
+
+    /**
+     * Metode ini menghitung total biaya penyewaan mobil.
+     *
+     * @return Total biaya penyewaan.
+     */
+    public double hitungTotalBiaya() {
+        return biayaSewa * durasiSewa;
+    }
+
+    /**
+     * Metode utama untuk menampilkan informasi penyewaan mobil.
+     *
+     * @param args Argumen baris perintah (tidak digunakan dalam contoh ini).
+     */
+    public static void main(String[] args) {
+        String pelanggan = "satrio";
+        String mobil = "Toyota ";
+        double biayaSewa = 100.0;
+        int durasiSewa = 3;
+
+        double totalBiaya = biayaSewa * durasiSewa;
+
+        System.out.println("Selamat datang, " + pelanggan + "!");
+        System.out.println("Anda telah menyewa mobil " + mobil + ".");
+        System.out.println("Durasi sewa: " + durasiSewa + " hari");
+        System.out.println("Biaya sewa per hari: $" + biayaSewa);
+        System.out.println("Total biaya: $" + totalBiaya);
+
+    }
+}
---
+++ src/AutocompleteAndTemplates/README.md
@@ -0,0 +1,42 @@
+# Aplikasi Penyewaan Mobil
+
+Aplikasi Penyewaan Mobil adalah proyek Java sederhana yang memungkinkan pengguna untuk menghitung total biaya penyewaan mobil berdasarkan nama pelanggan, jenis mobil yang disewa, biaya sewa per hari, dan durasi penyewaan dalam hari.
+
+## Cara Menggunakan Aplikasi
+
+1. Pastikan Anda memiliki Java Development Kit (JDK) terinstal di komputer Anda.
+2. Klon repositori ini ke komputer Anda dengan menjalankan perintah berikut:
+
+   shell
+   git clone https://github.com/username/repo-fork.git
+
+
+3. Buka direktori proyek:
+
+   shell
+   cd repo-fork
+
+
+4. Jalankan program dengan menjalankan kelas `MobilSewa` melalui perintah berikut:
+
+   shell
+   java AutocompleteAndTemplates.MobilSewa
+
+
+5. Ikuti instruksi program untuk memasukkan informasi penyewaan, seperti nama pelanggan, jenis mobil, biaya sewa per hari, dan durasi penyewaan. Hasilnya akan menampilkan informasi penyewaan, termasuk total biaya.
+
+## Contoh Penggunaan
+
+Misalnya, Anda ingin menyewa mobil dengan informasi berikut:
+- Nama pelanggan: Satrio
+- Jenis mobil: Toyota
+- Biaya sewa per hari: RP 100.000
+- Durasi penyewaan: 3 hari
+
+## Kontribusi
+
+Jika Anda ingin berkontribusi pada proyek ini atau melaporkan masalah, silakan buka [issue](https://github.com/username/repo-fork/issues) atau buat [pull request](https://github.com/username/repo-fork/pulls) ke repositori ini.
+
+## Lisensi
+
+Proyek ini dilisensikan di bawah [MIT License](LICENSE).
